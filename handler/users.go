b7a28d7848375d3db40cb48c967885091b76ccb5
mark-marushak <markk.9964@gmail.com>
2022-06-16 17:55:40 +0300

    create api handlers

---
+++ handler/users.go
@@ -0,0 +1,27 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func GetListUsers(c echo.Context) {
+
+}
+
+func CreateUser(c echo.Context) {
+
+}
+
+func GetUserByID(c echo.Context) {
+
+}
+
+func UpdateUser(c echo.Context) {
+
+}
+
+func DeleteUser(c echo.Context) {
+
+}
+
+func RestoreDeletedUser(c echo.Context) {
+
+}
